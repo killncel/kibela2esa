9ca315805be70f2d671f1692e19cc2b239366187
Yusuke Nakamura <yusuke1994525@gmail.com>
2019-01-28 03:14:21 +0900

    Replace kibela image url

--- kibera.rb
+++ kibera.rb
@@ -1,8 +1,10 @@
 require 'front_matter_parser'
+require 'nokogiri'
 
 module Kibela
   TEAM = 'unasuke'
   TITLE_PATTERN = %r[\A.*/kibela-#{TEAM}-\d+/(?<kind>wikis|blogs)/(?<path>[[:print:]]*/)(?<id>\d)+-(?<name>[[:print:]]*)\.md\z]
+  ATTACHMENT_PATTERN = %r[^(\.\./)*attachments/(?<attachment_name>\d+\.png)]
 
   class Note
     attr_accessor :name, :category, :body, :frontmatter
@@ -26,6 +28,15 @@ module Kibela
 
     def wiki?
       @kind == :wikis
+    end
+
+    def replace_attachment_names(attachment_list)
+      parsed_body = Nokogiri::HTML(@body)
+      parsed_body.css('img').map do |elem|
+        match = ATTACHMENT_PATTERN.match(elem.attributes['src'].value)
+        # 文中に出現するkibelaの画像URLをesaの画像URLに置換する
+        @body.gsub!(elem.attributes['src'], attachment_list[match['attachment_name']].esa_path) if match
+      end
     end
   end
 
